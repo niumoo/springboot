aaede8a6d39f4b1b47b7aabfe326324590b79011
niumoo <niumoo@126.com>
2018-08-24 16:09:54 +0800

    springboot hello

--- springboot-hello/src/main/java/net/codingme/boot/BootApplication.java
+++ springboot-hello/src/main/java/net/codingme/boot/BootApplication.java
@@ -2,9 +2,15 @@ package net.codingme.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * EnableScheduling 用于启动定时器
+ */
+
+@EnableScheduling
 @SpringBootApplication
 public class BootApplication {
 
