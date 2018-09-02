fb814c54ae5b3c79f0c1ced9ee21071be933b7a1
niumoo <niumoo@126.com>
2018-09-02 22:24:51 +0800

    springboot-hello

--- springboot-hello/src/main/java/net/codingme/boot/config/WebMvcConfig.java
+++ springboot-hello/src/main/java/net/codingme/boot/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.ArrayList;
@@ -13,15 +14,19 @@ import java.util.List;
 
 /**
  * <p>
- * 1.使用FastJSON
- * 2.配置时间格式化
- * 3.解决中文乱码
+ * 注意：在springboot2.0之前使用 extends WebMvcConfigurerAdapter 来实现功能
+ * 但是在springboot2.0 中应使用WebMvcConfigurer
+ * 原因可以看WebMvcConfigurerAdapter注释
  *
  * @Author niujinpeng
  * @Date 2018/8/3 23:15
+ * @see WebMvcConfigurerAdapter
+ * 1.使用FastJSON
+ * 2.配置时间格式化
+ * 3.解决中文乱码
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
