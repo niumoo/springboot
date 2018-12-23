ad6e034e7ca583f8b33d9583718b979a5cab3ff8
niumoo <niumoo@126.com>
2018-12-23 22:06:09 +0800

    springboot-properties 配置文件

---
+++ springboot-properties/src/test/java/net/codingme/boot/HelloApplicationTests.java
@@ -0,0 +1,70 @@
+package net.codingme.boot;
+
+import net.codingme.boot.domain.proper.Person;
+import net.codingme.boot.domain.proper.PersonSource;
+import net.codingme.boot.domain.proper.PersonValue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private Person person;
+    @Autowired
+    private PersonValue personValue;
+    @Autowired
+    private PersonSource personSource;
+
+    /**
+     * 模拟请求测试
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Greetings from Spring Boot!"));
+    }
+
+    /**
+     * 测试@ConfigurationProperties
+     */
+    @Test
+    public void testPersion() {
+        System.out.println(person);
+    }
+
+    /**
+     * 测试@Value 引入配置值
+     */
+    @Test
+    public void testPersionValue() {
+        System.out.println(personValue);
+    }
+
+    /**
+     * 测试#PropertySource 引入配置值
+     */
+    @Test
+    public void testPersionSource() {
+        System.out.println(personSource);
+    }
+
+}
