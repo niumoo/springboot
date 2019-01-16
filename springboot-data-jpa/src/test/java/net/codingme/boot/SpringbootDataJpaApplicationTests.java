c5d3ca5cd43bc2f355706bf4a532ea48e16bcb91
niumoo <niumoo@126.com>
2019-01-16 11:40:20 +0800

    springboot-data-jpa JPA 和 Druid

--- springboot-data-jpa/src/test/java/net/codingme/boot/SpringbootDataJpaApplicationTests.java
+++ springboot-data-jpa/src/test/java/net/codingme/boot/SpringbootDataJpaApplicationTests.java
@@ -2,15 +2,23 @@ package net.codingme.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootDataJpaApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
     }
 
 }
